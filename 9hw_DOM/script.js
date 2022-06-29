6e553b42027aa7fe01d5324b1e0de5e5392c1320
Mariia Kuzhydlo <kushnirmariia.m@gmail.com>
2022-06-29 18:51:25 +0300

    9hw update

--- 9hw_DOM/script.js
+++ 9hw_DOM/script.js
@@ -1,17 +1,13 @@
 const color = () => Math.floor(Math.random() * 256);
 const getRandomColor = () => `rgb(${color()} ${color()} ${color()})`;
 
-function generateBlocks() {
+function generateBlocksInterval() {
 	const colorsBlock = document.querySelector('.container');
 	for (let i = 1; i <= 25; i++) {
 		colorsBlock.insertAdjacentHTML('afterbegin', '<div class="square"></div>')
 	}
 	const square = document.querySelectorAll('.square');
 	square.forEach((el) => el.style.backgroundColor = getRandomColor());
-}
-
-function generateBlocksInterval() {
-	const square = document.querySelectorAll('.square');
 	square.forEach((el) => setInterval(() => {
 		el.style.backgroundColor = getRandomColor();
 	}, 1000))
